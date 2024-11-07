94cae5ba5bdd247e569eadae1e49a5508be9742b
Yi(Joy) Luo <lyluo@ucdavis.edu>
2024-11-07 14:22:38 -0800

    Update week_6_homework.R
    updated version in R

--- week_6_homework.R
+++ week_6_homework.R
@@ -29,22 +29,25 @@ gapminder %>%
     x = "Year",
     y = "Mean Life Expectancy"
   ) +theme_bw()
+
+
 ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
   geom_point(aes(color = continent), size = 0.5) +   
-  scale_x_log10() +   # Apply log scale to x-axis
-  geom_smooth() +     # Add a smoothing line
+  scale_x_log10() + # Apply log scale to x-axis
+  geom_smooth() + # Add a smoothing line
   labs(method = 'lm', color = 'black', linetype = 'dashed') +
   theme_bw()
-##to size the points in proportion to the population of the country
-ggplot(data= gapminder, aes(x = gdpPercap, y = lifeExp, size=population)) +
-  geom_point()+
-  geom_smooth(method = 'lm', color = 'black', linetype = 'dashed') +
-  theme_bw()
+
 
 ##select five countries
-gapminder_five_countries <- gapminder %>%
-  filter(country %in% c("Brazil", "China", "El Salvador", "Niger", "United States"))
 ##the data points in the backgroud using geom_jitter. 
 ##Label the X and Y axis with “Country” and “Life Expectancy” and title the plot “Life Expectancy of Five Countries”.
+gapminder_five_countries <- gapminder %>%
+  filter(country %in% c("Brazil", "China", "El Salvador", "Niger", "United States")) %>%
+  ggplot(aes(x= country, y= lifeExp)) + 
+  geom_boxplot()+geom_jitter(alpha = 0.5, color = "blue") +
+  labs(title = "Life Expectancy of Five Countries", x= "country", y="Life Expenctancy" ) +
+  theme_bw()
+gapminder_five_countries    
 
 
